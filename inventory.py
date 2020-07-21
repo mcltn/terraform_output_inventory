335b6e954ae3930f54f8959f49b2c396beedf223
Matt Colton <mcolton@us.ibm.com>
2020-07-21 10:41:10 -0500

    initial commit

---
+++ inventory.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import sys, json
+from splitstream import splitfile
+
+# use stdin to parse
+if not sys.stdin.isatty():
+    input_stream = sys.stdin
+else:
+    message = 'Expecting json to be piped in.'
+    raise IndexError(message)
+
+try:
+    output_filename = sys.argv[1]
+except IndexError:
+    message = 'Need a filename to save output to.'
+    raise IndexError(message)
+
+json_inv = {}
+for jsonstr in splitfile(sys.stdin, format="json"):
+    json_inv = json.loads(jsonstr)
+
+with open(output_filename, "w") as f:
+    # Create [allservers]
+    f.write("[allservers]\n")
+    for k in json_inv.keys():
+        for v in json_inv[k]["value"]:
+            f.write(v + "\n")
+    f.write("\n")
+
+    # Create sections for each Key
+    for k in json_inv.keys():
+        f.write("[" + k + "]\n")
+        for v in json_inv[k]["value"]:
+            f.write(v + "\n")
+        f.write("\n")
